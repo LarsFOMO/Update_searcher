3cdc14ee18999d2a784a9017c514ae54131da298
LarsFOMO <90476796+LarsFOMO@users.noreply.github.com>
2022-07-13 11:43:01 +0200

    Add files via upload

--- updateCode.py
+++ updateCode.py
@@ -4,6 +4,7 @@ import time
 import git
 import datetime
 from github import Github
+from pygit2 import clone_repository
 from os import path
 
 
@@ -16,17 +17,18 @@ try:
 except Exception:
     print("Fehler (0)")
 
+#   Abänderungsdatum der lokalen Datei
 try:
-    mod_date = os.path.getmtime('/home/lars/Dokumente/Test/TestPro')    #   Unixzeit (Start 1970)
+    mod_date = os.path.getmtime('/home/lars/Dokumente/Test/TestPro')        #   Unixzeit (Start 1970)
     #print(mod_date)
-    readable_date = datetime.datetime.fromtimestamp(mod_date)           #   Richtige Zeitrechnung
+    readable_date = datetime.datetime.fromtimestamp(mod_date)               #   Richtige Zeitrechnung
     print('\nLokal Changed on:', readable_date)  
     local_change_date = readable_date
 
 except Exception:
     print("Fehler (1)")
 
-
+#   Letztes Commit Datum
 try:
     master = my_repo.get_branch("master")
     sha_com = master.commit
@@ -39,31 +41,40 @@ try:
 except Exception:
     print("Fehler (2)")
 
+#   Prüft ob neuste Version auf Lokal
 try:
     if(last_commit_date > local_change_date):
         print("True")
     elif(last_commit_date < local_change_date):
         print("False")
+        #exit(0)
         
 except Exception:
     print("Fehler (3)")
 
-#all_commits = my_code.get_commits()
-#print(all_commits)
-
-# Update durchführen Abfrage
-eingabe_ok = 1                                                          #   Check ob gültige Eingabe
+#   Update durchführen (Abfrage)?
+eingabe_ok = 1                                                              #   Check ob gültige Eingabe
 while(eingabe_ok == 1):
     antwort = input("Ein neues Update ist verfügbar.Möchten Sie es installieren?\n(J,N)\n")
     if(antwort == "J"):
 
+#   Cloned Repo auf Lokal
+        try:
+            repo_URL = 'https://github.com/LarsFOMO/Test-Update.git'
+            local_path = '/home/lars/Dokumente/Test'
+            repoClone = clone_repository(repo_URL, local_path)
+
+        except Exception:
+            print("Fehler (4)")
+
         #for reposi in my_git.get_user().get_repos():
         #    print(reposi.name)
-            eingabe_ok = 0
 
-        #contents = my_repo.get_contents("")                         #   Enthaltene Dateien ausgeben
+        eingabe_ok = 0
+
+        #contents = my_repo.get_contents("")                                #   Enthaltene Dateien ausgeben
         #for content_file in contents:
-        #   print(content_file)
+            #print(content_file)
 
     elif(antwort == "N"):
         eingabe_ok = 0
@@ -72,4 +83,3 @@ while(eingabe_ok == 1):
     elif((antwort != "J") and (antwort != "N")):
         print("Keine gültige Eingabe. Wiederholen Sie die Eingabe")
 
-#return 0
