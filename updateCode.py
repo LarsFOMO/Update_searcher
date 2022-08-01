555fc0e09d723d9585cd1bf48eb2b752a69a807b
LarsFOMO <90476796+LarsFOMO@users.noreply.github.com>
2022-08-01 12:15:18 +0200

    Add files via upload

--- updateCode.py
+++ updateCode.py
@@ -12,6 +12,7 @@ import subprocess
 from subprocess import Popen
 import gitlab
 from subprocess import check_output
+import dateutil.parser as parser
 
 
 #   ACHTUNG[1]: Auf ein privates Repository kann nur mit einem Access-Token zugegriffen werden
@@ -24,10 +25,10 @@ from subprocess import check_output
 fehler = 0
 #   Check ob Update verfügbar 
 try:
-    #my_git = Github("LarsFOMO", "2003Lars")                                #   Login mit User+PW
-    #my_git = Github("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")     #Gitlab("NE2Ffawd5s3HgE3XcXfn")#    #   Login mit Token
-    gl = gitlab.Gitlab(url='http://alpmine.synology.me:8080',private_token='NE2Ffawd5s3HgE3XcXfn')
-    #my_repo = my_git.get_repo("LarsFOMO/Test-Update")       #('Code-Tester')                #   Repo auswählen
+    #my_git = Github("LarsFOMO", "2003Lars")                                                            #   Login mit User+PW
+    #my_git = Github("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")                                        #   Login mit Token
+    gl = gitlab.Gitlab(url='http://alpmine.synology.me:8080',private_token='')
+    #my_repo = my_git.get_repo("LarsFOMO/Test-Update")       #('Code-Tester')                           #   Repo auswählen
     gl.auth()
     project = gl.projects.get('paulsner_lars/Test')
 
@@ -37,32 +38,30 @@ except Exception:
 
 #   Abänderungsdatum der lokalen Datei
 try:
-    mod_date = os.path.getmtime('/home/lars/Dokumente/Test')                #   Unixzeit (Start 1970)
-    #print(mod_date)
-    readable_date = datetime.datetime.fromtimestamp(mod_date)               #   Richtige Zeitrechnung
+    mod_date = os.path.getmtime('/home/lars/Dokumente/Test')                                            #   Unixzeit (Start 1970)
+    readable_date = datetime.datetime.fromtimestamp(mod_date)                                           #   Richtige Zeitrechnung
     t_time = readable_date
-    print('\nLokal Changed on:', t_time.strftime("%d.%m.%Y, %H:%M:%S"))
+    localTime = t_time.strftime("  %d.%m.%Y, %H:%M:%S")
+    print('\nLokal changed on:', localTime)
     local_change_date = readable_date
-
+    localTimestamp = datetime.datetime.timestamp(t_time)
+    print(localTimestamp)
+    
 except Exception:
     print("Fehler (1)")
     fehler = 1
 
-#   Letztes Commit Datum
-#repo_url = 'http://alpmine.synology.me:8080/paulsner_lars/Test.git'
-#process = subprocess.Popen(["git", "ls-remote", repo_url], stdout=subprocess.PIPE)
-date_string = check_output('git log -1 --pretty=format:"%ci"'.split()).decode()
-print(date_string)
+#   Abänderungsdatum des Repositories
+#   Für genauere Infos zu 'last_activity_at': https://gitlab.com/gitlab-org/gitlab/-/issues/20952 
 try:
-    print(project.events.list(10))
-    master = my_repo.get_branch("master")
-    sha_com = master.commit
-    commit = my_repo.get_commit(sha=sha_com.sha)
-    hours = 2
-    hours_dazu = datetime.timedelta(hours = hours)
-    last_commit_date = commit.commit.author.date + hours_dazu
-    t2_time = last_commit_date
-    print("Last Commit:", t2_time.strftime("     %d.%m.%Y, %H:%M:%S"))
+    newtime = project.last_activity_at
+    date = parser.parse(newtime)                                                                        #   Zeit ist um 2h verschoben
+    addhours = 2
+    updatedTime = date + datetime.timedelta(hours= addhours)                                            #   Zeit anpassen
+    repoTime = updatedTime.strftime("    %d.%m.%Y, %H:%M:%S")
+    print('Last Update on:', repoTime)
+    repoTimestamp = datetime.datetime.timestamp(updatedTime)
+    print(repoTimestamp)    
     
 except Exception:
     print("Fehler (2)")
@@ -70,9 +69,9 @@ except Exception:
 
 #   Prüft ob neuste Version auf Lokal
 try:
-    if(last_commit_date > local_change_date):
-        print("Ein neues Update ist verfügbar")
-    elif(last_commit_date < local_change_date):
+    if(repoTimestamp > localTimestamp):
+        print("Es ist ein neues Update verfügbar...")
+    elif(repoTimestamp < localTimestamp):
         print("Sie haben bereits die neuste Version der Software auf Ihrem Gerät")
         exit(0)
         
@@ -81,18 +80,18 @@ except Exception:
     fehler = 1
 
 #   Update durchführen (Abfrage)?
-eingabe_ok = 1                                                              #   Check ob gültige Eingabe
+eingabe_ok = 1                                                                                          #   Check ob gültige Eingabe
 while(eingabe_ok == 1):
     antwort = input("Möchten Sie es installieren?\n(J,N)\n")
     if(antwort == "J"):
 
 #   Cloned Repo auf Lokal
-        tmp_path = '/home/lars/Dokumente/Foldertmp' #"/home/lars/Dokumente/Folder_tmp"  
-        local_dest_path = '/home/lars/Dokumente/Test'  #'/home/lars/Dokumente/Test'                       #   Zielpfad                      
-        os.mkdir(tmp_path)                                                  #   Neuen Ordner erstellen
+        tmp_path = '/home/lars/Dokumente/Foldertmp'   
+        local_dest_path = '/home/lars/Dokumente/Test'                                                   #   Zielpfad                      
+        os.mkdir(tmp_path)                                                                              #   Neuen Ordner erstellen
         
        ## repo_URL = 'https://github.com/LarsFOMO/Test-Update.git'
-        git_url = 'http://alpmine.synology.me:8080/paulsner_lars/Test'#'http://gitlab-goe/paulsner_lars/test.git'
+        git_url = 'http://alpmine.synology.me:8080/paulsner_lars/Test'                                  
 
         try:
             #Repo.clone_from(git_url,tmp_path)
@@ -100,15 +99,15 @@ while(eingabe_ok == 1):
             git.Repo.clone_from(git_url,tmp_path)
             
             #Popen(['git','clone',git_url,tmp_path])
-           ## Repo.clone_from(repo_URL,tmp_path)                          #   In tmp_path kopieren
+           ## Repo.clone_from(repo_URL,tmp_path)                                                        #   In tmp_path kopieren
         except Exception:
             print('Fehler (4)')
             fehler = 1
 
 
-        distutils.dir_util.copy_tree(tmp_path,local_dest_path)              #   Überschreibe alte Verion
+        distutils.dir_util.copy_tree(tmp_path,local_dest_path)                                          #   Überschreibe alte Verion
 
-        shutil.rmtree(tmp_path,ignore_errors=True)                                           #   tmp Ordner löschen  
+        shutil.rmtree(tmp_path,ignore_errors=True)                                                      #   tmp Ordner löschen  
 
         eingabe_ok = 0
         if(fehler == 0):
